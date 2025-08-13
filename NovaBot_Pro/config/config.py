4525843c6a42d65dc8cc610e4d6152b34e821182
omran-jo <omranhk815@gmail.com>
2025-08-13 07:55:51 +0300

    config.py

--- NovaBot_Pro/config/config.py
+++ NovaBot_Pro/config/config.py
@@ -1,7 +1,8 @@
-TOKEN = "6635657338:AAEwvftZpWgDfqoEmWTYhkaXYArOkVVN6aA"  # من @BotFather
+TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  # من @BotFather
 ADMIN_ID = 6635657338                        # من @userinfobot
-GEMINI_API_KEY = "AIzaSyCxx6wIBRwiMZIIaab6-u4y_5N_V05Bsug"     # من aistudio.google.com
+GEMINI_API_KEY = "AIzxxxxxxxxxxxxxxxxxxxxxxxxxx"     # من aistudio.google.com
 
 CHANNELS = [
     {"id": -1001782887536, "username": "@sadwriter12"}
+
 ]
